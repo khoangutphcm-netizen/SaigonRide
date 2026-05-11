07378a36ecf1e2c90ee54a33ddbb967f1d38e290
nguyenhoakhang <nghoakhang@gmail.com>
2026-05-11 21:34:35 +0700

    update database connection

--- SaigonRide/Program.cs
+++ SaigonRide/Program.cs
@@ -3,47 +3,66 @@ using SaigonRide.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.DataProtection;
 using System.IO;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
- 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Lấy connection string từ appsettings hoặc environment variables
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")?.Trim()
+    ?? Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+// Nếu không có connection string, throw exception
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found in configuration or environment variables.");
+}
+
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
- 
+    options.UseMySql(connectionString, serverVersion));
+
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddControllersWithViews()
     .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix);
- 
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
-        options.LoginPath = "/Account/Login";  
+        options.LoginPath = "/Account/Login";
         options.AccessDeniedPath = "/Account/AccessDenied";
+        options.ExpireTimeSpan = TimeSpan.FromDays(30);
+        options.SlidingExpiration = true;
     });
-builder.Services.AddScoped<SaigonRide.Services.RentalService>(); 
+
+builder.Services.AddScoped<SaigonRide.Services.RentalService>();
+
 builder.Services.AddDataProtection()
     .PersistKeysToFileSystem(new DirectoryInfo(Path.Combine(builder.Environment.ContentRootPath, "TempKeys")))
     .SetApplicationName("SaigonRide");
+
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-    // Cài đặt thời gian sống của Session (VD: 2 tiếng, đủ dài cho 1 cuốc xe)
     options.IdleTimeout = TimeSpan.FromHours(2);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
-var app = builder.Build(); 
+
+var app = builder.Build();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
- 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseSession();
-var viCulture = new System.Globalization.CultureInfo("vi"); 
+
+var viCulture = new System.Globalization.CultureInfo("vi");
 viCulture.NumberFormat.NumberDecimalSeparator = ".";
 viCulture.NumberFormat.CurrencyDecimalSeparator = ".";
 
@@ -58,11 +77,18 @@ var localizationOptions = new RequestLocalizationOptions
 
 app.UseRequestLocalization(localizationOptions);
 
-app.UseAuthentication(); 
+app.UseAuthentication();
 app.UseAuthorization();
-app.UseMiddleware<SaigonRide.Middlewares.BanCheckMiddleware>(); 
+app.UseMiddleware<SaigonRide.Middlewares.BanCheckMiddleware>();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Intro}/{action=Index}/{id?}");  
+    pattern: "{controller=Intro}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
